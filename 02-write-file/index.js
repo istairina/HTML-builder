01f015c1412b14d9ee3584dc050a34404bc77b92
Irina Migunova <ista.irina@gmail.com>
2022-11-03 00:53:40 +0300

    feat: finish second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('readline');
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+let writeText = function (answer) {
+
+  if (answer == 'exit') {
+    console.log('Bye-bye! Come back soon');
+    rl.close();
+  } else {
+    answer += '\n';
+    fs.appendFile('02-write-file/output.txt', answer, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+}
+
+rl.question('What is your favorite city?\n', (answer) => {
+  writeText(answer);
+  rl.on('line', (answer) => {
+    writeText(answer);
+  });
+});
+
+rl.on('SIGINT', () => {
+  console.log('\nBye-bye! Come back soon');
+  rl.close();
+});
