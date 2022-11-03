7f673381a67a45017307fa1268bd5c7d5c484c96
Irina Migunova <ista.irina@gmail.com>
2022-11-03 23:28:58 +0300

    feat: finish fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
 
+let folder = '04-copy-directory/files';
+let folderDist = '04-copy-directory/files-copy';
+
+fs.mkdir(folderDist, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+
+fs.readdir(folderDist, (err, file) => {
+  if (err) console.log(err);
+  for (i = 0; i < file.length; i++) {
+    fs.unlink(`${folderDist}/${file[i]}`, (err) => {
+      if (err) console.log(err);
+    });
+  }
+});
+
+
+fs.readdir(folder, (err, file) => {
+  if (err) console.log(err);
+  for (i = 0; i < file.length; i++) {
+    fs.createReadStream(`${folder}/${file[i]}`).pipe(fs.createWriteStream(`${folderDist}/${file[i]}`));
+}
+});
