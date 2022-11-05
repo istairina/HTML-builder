a828d054698fc1c5e2250c60d662510163726818
Irina Migunova <ista.irina@gmail.com>
2022-11-06 00:54:49 +0300

    feat: rewrite fourth task with promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,20 +7,31 @@ fs.mkdir(folderDist, { recursive: true }, (err) => {
     if (err) throw err;
   });
 
-
-fs.readdir(folderDist, (err, file) => {
+/*fs.readdir(folderDist, (err, file) => {
   if (err) console.log(err);
   for (i = 0; i < file.length; i++) {
     fs.unlink(`${folderDist}/${file[i]}`, (err) => {
       if (err) console.log(err);
     });
   }
-});
+});*/
 
+fs.promises.readdir(folderDist)
 
-fs.readdir(folder, (err, file) => {
-  if (err) console.log(err);
+.then (function(file) {
   for (i = 0; i < file.length; i++) {
-    fs.createReadStream(`${folder}/${file[i]}`).pipe(fs.createWriteStream(`${folderDist}/${file[i]}`));
-}
-});
+    fs.unlink(`${folderDist}/${file[i]}`, (err) => {
+      if (err) console.log(err);
+    });
+  }
+})
+
+.then (function() {
+  fs.readdir(folder, (err, file) => {
+    if (err) console.log(err);
+    for (i = 0; i < file.length; i++) {
+      fs.createReadStream(`${folder}/${file[i]}`).pipe(fs.createWriteStream(`${folderDist}/${file[i]}`));
+  }
+  });
+})
+
